109be8cf952e8ae66ce2319f920cdc64fc19cdb5
agabor <angyalgabor@outlook.com>
2017-05-17 14:56:09 +0200

    users table

--- server/app/Console/Kernel.php
+++ server/app/Console/Kernel.php
@@ -32,4 +32,15 @@ class Kernel extends ConsoleKernel
     {
         //
     }
+
+    /**
+     * Terminate the application.
+     *
+     * @param  \Symfony\Component\Console\Input\InputInterface  $input
+     * @param  int  $status
+     * @return void
+     */
+    public function terminate($input, $status)
+    {
+    }
 }

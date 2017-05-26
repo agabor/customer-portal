4cb22e9a5799a8c0eccb29b29de9bd5395d8f9e7
agabor <angyalgabor@outlook.com>
2017-05-26 11:05:58 +0200

    choose video

--- client/src/app/components/project/videos.component.ts
+++ client/src/app/components/project/videos.component.ts
@@ -1,14 +1,29 @@
 import { Component } from '@angular/core';
-import {Presenter} from "../../logic/presenter";
-import {Project} from "../../../swagger/model/Project";
-import {Image} from "../../../swagger/model/Image";
-import {Modal} from "../../ui/modal";
-
+import {VgAPI} from 'videogular2/core';
 @Component({
     selector: 'project-videos',
     templateUrl: './videos.component.html'
 })
 export class VideosComponent {
+    videos: Video[] = [
+        new Video('Install App with Testflight', 'http://localhost:8000/testflight.mp4'),
+        new Video('Play Store Beta testing', 'http://localhost:8000/sample.mp4'),
+        new Video('Basic usage of the app', 'http://localhost:8000/testflight.mp4')
+    ];
+    currentVideo: Video[] = [this.videos[1]];
+    api:VgAPI;
+    select(video: Video){
+        this.currentVideo[0] = video;
+    }
 
+    onPlayerReady(api:VgAPI) {
+        this.api = api;
+    }
+}
+
+class Video{
+    constructor (public name:string, public link:string){
+
+    }
 
 }
